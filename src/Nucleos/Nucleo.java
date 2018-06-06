23b56815812090a1b76677ddadfa5e326e8755ed
B32540 <javialofer@hotmail.com>
2018-06-06 15:40:42 -0600

    Esqueleto ejecutar instruccion

--- src/Nucleos/Nucleo.java
+++ src/Nucleos/Nucleo.java
@@ -4,6 +4,37 @@ public class Nucleo {
     private int id;
     private Thread[] threads;
 
+    public void ejecutar_instruccion(int i){
+        switch(i){
+            case 8:
+                break;
+            case 32:
+                break;
+            case 34:
+                break;
+            case 12:
+                break;
+            case 14:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 3:
+                break;
+            case 2:
+                break;
+            case 35:
+                break;
+            case 43:
+                break;
+            case 63:
+                break;
+        }
+
+    }
+
+
     public int daddi(int RF1, int RF2, int RD){
         RF2=RF1+RD;
         return RF2;
