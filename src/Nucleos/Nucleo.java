9e31b3b9354bfe6f8ce57ae5c0d0eaa92a6bb3f2
HuskyDM <javialofer@hotmail.com>
2018-06-10 14:33:42 -0600

    Triviales hechas, falta manejo de quantum

--- src/Nucleos/Nucleo.java
+++ src/Nucleos/Nucleo.java
@@ -1,4 +1,5 @@
 package Nucleos;
+import Estructuras_Datos.Hilo;
 import Estructuras_Datos.Instruccion;
 import MVC.Simulacion;
 
@@ -6,11 +7,12 @@ public class Nucleo implements Runnable {
 
     protected Simulacion simulacion;
     protected int id;
-
+    public Hilo hiloEjecucion;
 
     public Nucleo(Simulacion simulacion, int id) {
         this.simulacion = simulacion;
         this.id = id;
+        hiloEjecucion=simulacion.pedirHiloCola();
     }
   
   
@@ -18,23 +20,31 @@ public class Nucleo implements Runnable {
         int[] ejecuccion = instruccion.getPalabra();
         switch (ejecuccion[0]) {
             case 8: //Daddi
-                //this.simulacion...
+                daddi(hiloEjecucion.getRegistro(ejecuccion[1]),hiloEjecucion.getRegistro(ejecuccion[2]),hiloEjecucion.getRegistro(ejecuccion[3]));
                 break;
             case 32: //Dadd
+                dadd(hiloEjecucion.getRegistro(ejecuccion[1]),hiloEjecucion.getRegistro(ejecuccion[2]),hiloEjecucion.getRegistro(ejecuccion[3]));
                 break;
             case 34: //Dsub
+                dsub(hiloEjecucion.getRegistro(ejecuccion[1]),hiloEjecucion.getRegistro(ejecuccion[2]),hiloEjecucion.getRegistro(ejecuccion[3]));
                 break;
             case 12: //Dmul
+                dmul(hiloEjecucion.getRegistro(ejecuccion[1]),hiloEjecucion.getRegistro(ejecuccion[2]),hiloEjecucion.getRegistro(ejecuccion[3]));
                 break;
             case 14: //Ddiv
+                ddiv(hiloEjecucion.getRegistro(ejecuccion[1]),hiloEjecucion.getRegistro(ejecuccion[2]),hiloEjecucion.getRegistro(ejecuccion[3]));
                 break;
             case 4: //BEQZ
+                beqz(hiloEjecucion.getRegistro(ejecuccion[1]),hiloEjecucion.getRegistro(ejecuccion[3]));
                 break;
             case 5: //BNEZ
+                bnez(hiloEjecucion.getRegistro(ejecuccion[1]),hiloEjecucion.getRegistro(ejecuccion[3]));
                 break;
             case 3: //Jal
+                jal(hiloEjecucion.getRegistro(ejecuccion[1]), hiloEjecucion.getPc());
                 break;
             case 2: //JR
+                jr(hiloEjecucion.getRegistro(ejecuccion[1]));
                 break;
             case 35: //LW
                 break;
@@ -46,59 +56,57 @@ public class Nucleo implements Runnable {
     }
     /**Operations**/
 
-    public int daddi(int RF1, int RF2, int RD){
-        RF2=RF1+RD;
-        //hilo.pc+=4;
-        return RF2;
+    public void daddi(int RF1, int RF2, int RD){
+        int op=RF1+RD;
+        hiloEjecucion.setRegistro(RF2,op);
+        hiloEjecucion.setPc(hiloEjecucion.getPc()+4);
     }
 
-    public int dadd(int RF1, int RF2, int RD){
-        RF2= RF1+RD;
-        return RF2;
+    public void dadd(int RF1, int RF2, int RD){
+        int op=RF1+RD;
+        hiloEjecucion.setRegistro(RF2,op);
+        hiloEjecucion.setPc(hiloEjecucion.getPc()+4);
     }
 
-    public int dsub(int RF1, int RF2, int RD){
-        RF2=RF1-RD;
-        return RF2;
+    public void dsub(int RF1, int RF2, int RD){
+        int op=RF1-RD;
+        hiloEjecucion.setRegistro(RF2,op);
+        hiloEjecucion.setPc(hiloEjecucion.getPc()+4);
     }
 
-    public int dmul(int RF1, int RF2, int RD){
-        RF2=RF1*RD;
-        return RF2;
+    public void dmul(int RF1, int RF2, int RD){
+        int op=RF1*RD;
+        hiloEjecucion.setRegistro(RF2,op);
+        hiloEjecucion.setPc(hiloEjecucion.getPc()+4);;
     }
 
-    public int ddiv(int RF1, int RF2, int RD){
-        RF2=RF1/RD;
-        return RF2;
-
+    public void ddiv(int RF1, int RF2, int RD){
+        int op=RF1/RD;
+        hiloEjecucion.setRegistro(RF2,op);
+        hiloEjecucion.setPc(hiloEjecucion.getPc()+4);
     }
 
-    public int beqz(int RF, int RD){
-        int pc=0;
+    public void beqz(int RF, int RD){
         if(RF==0){
-            pc=4*RD;
+            hiloEjecucion.setPc(4*RD);
         }
-        return pc;
     }
 
-    public int bnez(int RF, int RD){
-        int pc=0;
+    public void bnez(int RF, int RD){
         if(RF!=0){
-            pc=4*RD;
+            hiloEjecucion.setPc(4*RD);
         }
-        return pc;
     }
 
-    public int jal(int RD, int PC){
-        int pc;
-        int R31=PC; //Deberia cambiar el registro R31 del contexto que lo pidio
-        pc=RD;
-        return pc;
+    public void jal(int RD, int PC){
+
+        hiloEjecucion.setRegistro(31,hiloEjecucion.getPc()); //Deberia cambiar el registro R31 del contexto que lo pidio, el pc actual
+        hiloEjecucion.setPc(RD);
+
     }
 
-    public int jr(int RF){
-        int pc=RF;
-        return  pc;
+    public void jr(int PC){
+        hiloEjecucion.setPc(PC);
     }
 
     public void lw(){ //Lleva override y super
@@ -113,9 +121,14 @@ public class Nucleo implements Runnable {
 
     }
 
+    public  void fin_quantum(){
+        this.simulacion.devolverHiloCola(hiloEjecucion);
+    }
+
     public void fallo_instrucciones(){}
 
     public void fallo_datos(){}
+
 /**TODO Arreglar esto luego**/
 //    public int[] getBloqueMemoria(int direccionMemoria) {
 //        int bloqueACargar[] = new int[4];
