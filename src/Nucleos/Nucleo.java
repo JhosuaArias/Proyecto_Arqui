85d13cd41f2203a35c839f3a64dd9225fb3caa84
JhosuaArias <jhosuarqui@hotmail.com>
2018-06-11 12:25:49 -0600

    Flujo de simulacion listo. Flujo de N1 empezado.

--- src/Nucleos/Nucleo.java
+++ src/Nucleos/Nucleo.java
@@ -118,8 +118,8 @@ public class Nucleo implements Runnable {
 
     }
 
-    public void fin(){ //Pone el hilo.esFin a true
-
+    public void fin(Hilo hilo){ //Pone el hilo.esFin a true
+        hilo.setEsFin(true);
     }
 
     public  void fin_quantum(){
@@ -129,60 +129,7 @@ public class Nucleo implements Runnable {
     public void fallo_instrucciones(){}
 
     public void fallo_datos(){}
-
-/**TODO Arreglar esto luego**/
-//    public int[] getBloqueMemoria(int direccionMemoria) {
-//        int bloqueACargar[] = new int[4];
-//        int numeroBloque = (direccionMemoria / 16);
-//
-//        if (numeroBloque > 24) //Utilizo la cache de instrucciones
-//        {
-//            for (int i = 0; i <= 3; ++i) {
-//                //Falta de arreglar
-//                bloqueACargar[i] = memoria.instrucciones[numeroBloque - 24][i];
-//            }
-//
-//            return bloqueACargar;
-//        }
-//
-//        //Utilizo la cache de Datos
-//        else {
-//
-//            for (int i = 0; i <= 3; ++i) {
-//                //Falta de arreglar
-//                bloqueACargar[i] = memoria.instrucciones[numeroBloque][i];
-//            }
-//
-//            return bloqueACargar;
-//        }
-//    }
-//
-//    public int getPalabraCache(int direccionMemoria, int nucleo)
-//    {
-//        int numeroBloque = (direccionMemoria / 16);
-//        int direccionPalabra = (direccionMemoria -  (16 * numeroBloque)) /  4;
-//
-//        if (nucleo==0) //Estoy en el nucleo 0
-//        {
-//            int posicionCache = (direccionMemoria / 16) % 8;
-//
-//            if ( numeroBloque>24) //Utilizo la cache de instrucciones
-//            {return cacheInstruccionesN0[posicionCache] [direccionPalabra];}
-//
-//            else {return cacheDatosN0[posicionCache] [direccionPalabra];}
-//            }
-//
-//        	else //Soy N1
-//            {
-//                int posicionCache = (direccionMemoria / 16) % 4;
-//
-//                if (numeroBloque>24) //Utilizo la cache de instrucciones
-//                {return cacheInstruccionesN1[posicionCache] [direccionPalabra];}
-//
-//                else {return cacheDatosN1[posicionCache] [direccionPalabra];}
-//                }
-//
-//    }
+    
 
     @Override
     public void run() {
